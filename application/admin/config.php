348157edd61fd56f30152d0edc5f7c892ad93633
rock-dav <15766485478@163.com>
2019-04-03 01:21:47 +0800

    后台模板调试

--- application/admin/config.php
+++ application/admin/config.php
@@ -1,8 +1,10 @@
 <?php
 
 return [
+    
     'view_replace_str' => [
         '__STATIC__' => 'public/static/admin',
+        '__HOST__' => $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . '/public/static/admin',
     ],
 
 
--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -1,14 +1,32 @@
 <?php
-
+/**
+ * 后台管理系统首页
+ */
 namespace app\admin\controller;
 
 class Index extends Base
 {
+    
     public function index()
     {
         
-
-
+        
+        
         return $this->fetch();
+    }
+
+    public function welcome(){
+
+        
+        // dump($_SERVER);exit;
+        return $this->fetch();
+    }
+    
+
+    public function test(){
+
+
+
+        echo 'test';exit;
     }
 }
