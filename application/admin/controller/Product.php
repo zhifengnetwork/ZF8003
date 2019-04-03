c581f7b7e22fbb2696694c7758ce54ac94fa59b2
zxz <1026853966@qq.com>
2019-04-03 12:08:36 +0800

    添加图片、产品后台模板文件

--- application/admin/controller/Product.php
+++ application/admin/controller/Product.php
@@ -4,7 +4,34 @@ namespace app\admin\controller;
 
 class Product extends Base
 {
+    /**
+     * 品牌
+     */
     public function brand()
+    {
+        return $this->fetch();
+    }
+
+    /**
+     * 分类
+     */
+    public function category()
+    {
+        return $this->fetch();
+    }
+
+    /**
+     * 添加分类
+     */
+    public function category_add()
+    {
+        return $this->fetch();
+    }
+
+    /**
+     * 产品列表
+     */
+    public function product_list()
     {
         return $this->fetch();
     }
