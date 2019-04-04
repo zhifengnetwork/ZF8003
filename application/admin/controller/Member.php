a49ff50b55aacdd5be6faf1ce5ec8cc362ef0c4a
zxz <1026853966@qq.com>
2019-04-04 15:09:02 +0800

    后台会员编辑

--- application/admin/controller/Member.php
+++ application/admin/controller/Member.php
@@ -43,12 +43,20 @@ class Member extends Base
     /**
      * 编辑会员
      */
-    public function eidt()
+    public function edit()
     {
-        $act = "eidt";
+        $id = isset($_GET['id']) ? intval($_GET['id']) : 0;
+        
+        if (!$id) {
+            echo "<script>alert('该用户不存在')</script>";
+            exit;
+        }
+        $act = "edit";
+        $user = Db::name('users')->where('id',$id)->field('password,openid,unionid,payment_password',true)->find();
         $group = Db::name('user_group')->select();
         
         $this->assign('act',$act);
+        $this->assign('info',$user);
         $this->assign('group',$group);
         return $this->fetch();
     }
@@ -62,9 +70,10 @@ class Member extends Base
         $member = Loader::validate('Member');
         $user = new Users;
         $return = ['status' => 0, 'msg' => '参数错误'];//初始化返回信息
+
         if ($data['act'] == "add") {
             if (!$member->scene('add')->check($data)) {
-                $return = ['status' => 0, 'msg' => $member->getError()];
+                $return = array('status' => 0, 'msg' => $member->getError());
             } else {
                 $is_distribut = ($data['is_distribut'] == 1) ? $data['is_distribut'] : 0;
                 $result = array(
@@ -84,6 +93,36 @@ class Member extends Base
                     $return = array('code' => 0, 'msg' => "添加失败！");
                 }
             } 
+        }
+
+        if ($data['act'] == "edit") {
+            if (!$member->scene('edit')->check($data)) {
+                $return = array('status' => 0, 'msg' => $member->getError());
+            } else {
+                $is_distribut = (intval($data['is_distribut']) == 1) ? intval($data['is_distribut']) : 0;
+                $status = (intval($data['status']) == 1) ? intval($data['status']) : 0;
+                $money = intval($data['money']);
+
+                $result = array(
+                    'nickname'       => $data['nickname'],
+                    'mobile'         => $data['mobile'],
+                    'email'          => $data['email'],
+                    'group_id'       => $data['group_id'],
+                    'is_distributor' => $is_distribut,
+                    'status'         => $status,
+                );
+
+
+                if ($money) {
+                    $result['money'] = $money;
+                }
+            }
+            $bool = false;
+            if ($bool) {
+                $return = array('code' => 1, 'msg' => "修改成功！");
+            } else {
+                $return = array('code' => 0, 'msg' => "修改失败！");
+            }
         }
 
         if ($data['act'] == "del") {
--- application/admin/validate/Member.php
+++ application/admin/validate/Member.php
@@ -11,17 +11,19 @@ class Member extends Validate
         'mobile'   => 'require|unique:users',
         'email'    => 'require|unique:users',
         'group_id' => 'require',
+        'money'    => 'number',
     ];
     
     //错误消息
     protected $message = [
-        'nickname.require'    => '请输入用户名',
-        'nickname.unique'     => '该用户名已存在',
-        'mobile.require'      => '请输入手机',
-        'mobile.unique'       => '该手机号已注册',
-        'email.require'       => '请输入邮箱',
-        'email.unique'        => '该邮箱已注册',
-        'group_id.require'    => '请输选择分组',
+        'nickname.require'    => '请输入用户名！',
+        'nickname.unique'     => '该用户名已存在！',
+        'mobile.require'      => '请输入手机！',
+        'mobile.unique'       => '该手机号已注册！',
+        'email.require'       => '请输入邮箱！',
+        'email.unique'        => '该邮箱已注册！',
+        'group_id.require'    => '请输选择分组！',
+        'money.number'        => '余额必须是数字！'
     ];
 
     //错误消息
@@ -32,6 +34,15 @@ class Member extends Validate
             'mobile' => 'require|unique:users,mobile^id',
             'email' => 'require|unique:users,email^id',
             'group_id' => 'require',
+        ],
+
+        //编辑
+        'edit'    => [
+            'nickname' => 'require|unique:users,nickname^id',
+            'mobile'   => 'require|unique:users,mobile^id',
+            'email'    => 'require|unique:users,email^id',
+            'group_id' => 'require',
+            'money'    => 'number',
         ],
 
         'set_pwd' => ['password'],
