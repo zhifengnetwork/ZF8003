2e1e90fe7f80c3c38face6743396d7ebc6248d07
670600971 <670600971@qq.com>
2019-04-11 11:03:41 +0800

    拉取底部

--- application/mobile/controller/Goods.php
+++ application/mobile/controller/Goods.php
@@ -7,6 +7,20 @@ class Goods extends Base
 {
     public function index()
     {
-
+        return $this->fetch();
     }
+    /**
+     * 分类列表显示
+     */
+    public function categoryList()
+    {
+        return $this->fetch();
+    }
+    /**
+     * 商品列表显示
+     */
+    public function goodsList()
+    {
+        return $this->fetch();
+    }   
 }
