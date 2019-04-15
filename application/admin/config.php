b5317328e9923ad874240ef99a1dd31ddb8878c7
Rock-dav <1576648548@163.com>
2019-04-15 11:35:12 +0800

    设置session域

--- application/admin/config.php
+++ application/admin/config.php
@@ -7,5 +7,22 @@ return [
         '__STATIC__' => 'public/static/admin',
         '__HOST__' => $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . '/public/static/admin',
     ],
+    
+    // +----------------------------------------------------------------------
+    // | 会话设置
+    // +----------------------------------------------------------------------
+
+    'session'                => [
+        'id'             => '',
+        // SESSION_ID的提交变量,解决flash上传跨域
+        'var_session_id' => '',
+        // SESSION 前缀
+        'prefix'         => 'admin',
+        // 驱动方式 支持redis memcache memcached
+        'type'           => '',
+        // 是否自动开启 SESSION
+        'auto_start'     => true,
+    ],
+
 
 ];
