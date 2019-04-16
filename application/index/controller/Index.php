4484646500103ce681fb2e1312badfd873d217bb
670600971 <670600971@qq.com>
2019-04-16 22:04:19 +0800

    注册和登录

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -2,7 +2,7 @@
 namespace app\index\controller;
 
 use think\Controller;
-
+use think\Db;
 class Index extends Base
 {
     // public function index()
@@ -22,4 +22,54 @@ class Index extends Base
     {
         return $this->fetch();
     }
+
+
+    # 发送注册码邮件
+    public function send_register_mail()
+    {
+        $register_id = isset($_POST['register_id']) ? trim($_POST['register_id']) : '';
+        $email = isset($_POST['email']) ? trim($_POST['email']) : '';
+
+        if ($register_id && $email) {
+
+            $user = Db::name('users')->where('email', $email)->find();
+            if ($user) {
+                return json(['status' => 0, 'msg' => '该账号已被注册！']);
+            }
+
+            $code = rand(100000, 999999);
+
+            $config = Db::query("select `name`,`value` from `zf_config` where `type` = 'email_setting'");
+            if (empty($config)) {
+                return json(['status' => 0, 'msg' => '系统未设置相关参数，请联系管理员']);
+            }
+            foreach ($config as $v) {
+                $conf[$v['name']] = $v['value'];
+            }
+
+            $param = [
+                'host'      => $conf['host'],
+                'username'  => $conf['username'],
+                'password'  => $conf['password'],
+                'secure'    => $conf['secure'],
+                'port'      => $conf['port'],
+                'nickname'  => $conf['nickname'],
+                'to'        => $email,
+                'title'     => $conf['register_title'],
+                'body'      => $conf['register_body'] ? str_replace('{{$code}}', $code, $conf['register_body']) : "<h1>注册码：$code</h1>",
+                'altbody'   => $conf['register_altbody'] ? str_replace('{{$code}}', $code, $conf['register_altbody']) : "注册码：$code",
+            ];
+
+            if ($this->base_send_mail($param)) {
+                $time = time();
+                Db::execute("delete from `zf_mail_code` where `type` = 'register' and `sn` = '$register_id'");
+                Db::execute("insert into `zf_mail_code` (`sn`,`email`,`code`,`addtime`,`type`) values ('$register_id', '$email', '$code', '$time', 'register')");
+                return json(['status' => 1]);
+            } else {
+                return json(['status' => 0, 'msg' => '注册码发送失败！']);
+            }
+        }
+        exit('null');
+    }
+
 }
--- application/index/controller/Login.php
+++ application/index/controller/Login.php
@@ -2,8 +2,9 @@
 namespace app\index\controller;
 
 use think\Controller;
-
-class Login extends Controller
+use think\Db;
+use think\Session;
+class Login extends Base
 {
     public function index()
     {
@@ -15,14 +16,131 @@ class Login extends Controller
      */
     public function login()
     {
+        if($_POST){
+            $email = isset($_POST['email']) ? trim($_POST['email']) : '';
+            $password = isset($_POST['password']) ? trim($_POST['password']) : '';
+            // $captcha = isset($_POST['captcha']) ? trim($_POST['captcha']) : '';
+
+            if(!check_email($email)){
+                return json(['status'=>0,'msg'=>'邮箱格式错误！']);
+            }
+            if(!$password){
+                return json(['status'=>0,'msg'=>'请填写登录密码！']);
+            }
+            // if(!captcha_check($captcha)){
+            //     return json(['status'=>0,'msg'=>'验证码错误！']);
+            // };
+            $user = Db::name('users')->where('email',$email)->find();
+            if(!$user){
+                return json(['status'=>0,'msg'=>'账户不存在！']);
+            }
+            if(pwd_encryption($password) != $user['password']){
+                return json(['status'=>0,'msg'=>'登录密码错误！']);
+            }
+
+            Session::set('user',$user);
+            $time = time();
+            $log_data = [
+                'user_id'   => $user['id'],
+                'type'  =>  'email',
+                'ip'    =>  $this->ip,
+                'client'    => $this->client,
+                'addtime'   => $time,
+            ];
+            Db::name('user_login_log')->insert($log_data);
+            Db::name('users')->where('id',$user['id'])->update(['login_time'=>$time]);
+
+            $re_url = Session::has('re_url') ? Session::get('re_url') : '/index/index/index';
+           
+            return json(['status'=>1,'msg'=>'登录成功，正在跳转...','url'=>$re_url]);
+
+            exit;
+        }
+        
+        // captcha_src();
         return $this->fetch();
     }
 
     /**
      * 注册
      */
-    public function register()
-    {
+    public function register(){
+        if($_POST){
+            $register_id = isset($_POST['register_id']) ? trim($_POST['register_id']) : '';
+            $email = isset($_POST['email']) ? trim($_POST['email']) : '';
+            $code = isset($_POST['code']) ? trim($_POST['code']) : '';
+            $password = isset($_POST['password']) ? trim($_POST['password']) : '';
+            $re_password = isset($_POST['re_password']) ? trim($_POST['re_password']) : '';
+            $captcha = isset($_POST['captcha']) ? trim($_POST['captcha']) : '';
+           
+            if(!$register_id){
+                return json(['status'=>0,'msg'=>'系统错误，请刷新页面后重试！']);
+            }
+            
+            if(!check_email($email)){
+                return json(['status'=>0,'msg'=>'邮箱格式错误！']);
+            }
+
+            $code_info = Db::name('mail_code')->where(['type'=>'register', 'sn'=>$register_id, 'code'=>$code])->find();
+            if(!$code_info){
+                return json(['status'=>0,'msg'=>'注册码错误！']);
+            }
+            if(!$password){
+                return json(['status'=>0,'msg'=>'请填写登录密码！']);
+            }
+
+            if(!captcha_check($captcha)){
+                return json(['status'=>0,'msg'=>'验证码错误！']);
+            };
+               
+
+            $user = Db::name('users')->where('email',$email)->find();
+            if($user){
+                return json(['status'=>0,'msg'=>'该账号已被注册！']);
+            }
+            
+            $time = time();
+            $ip = $this->ip;
+            $client = $this->client;
+
+            $inser_date = [
+                'email'     => $email,
+                'email_verification'    => 1,
+                'nickname'  => $email,
+                'password'  => pwd_encryption($password),
+                'register_method'   => 'email',
+                'register_time' => $time,
+                'login_time'    => $time,
+            ];
+            
+            $user_id = Db::name('users')->insertGetId($inser_date);
+
+            if($user_id){
+                Db::name('mail_code')->where(['type'=>'register', 'sn'=>$register_id, 'code'=>$code])->delete();
+
+                $user = Db::name('users')->find($user_id);
+                Session::set('user', $user);
+
+                $log_data = [
+                    'user_id'   => $user_id,
+                    'type'  =>  'email',
+                    'ip'    =>  $ip,
+                    'client'    => $client,
+                    'addtime'   => $time,
+                ];
+
+                Db::name('user_login_log')->insert($log_data);
+
+                $re_url = Session::has('re_url') ? Session::get('re_url') : '/index/index/index';
+
+                return json(['status'=>1,'msg'=>'注册成功，正在跳转...','url'=>$re_url]);
+            }
+
+            return json(['status'=>0,'msg'=>'注册失败，请重试！']);
+            exit;
+        }
+
+        $this->assign('register_id', md5(time().rand(1000,9999).rand(1000,9999)));
         return $this->fetch();
     }
 }
