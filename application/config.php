af825cca2637fed41e4c76dcc14c72a602e34fca
zxz <1026853966@qq.com>
2019-04-16 15:09:49 +0800

    修改验证码设置、登录获取用户信息

--- application/config.php
+++ application/config.php
@@ -252,7 +252,7 @@ return [
         // 验证码字体大小(px)
         'fontSize' => 15,
         // 是否画混淆曲线
-        'useCurve' => true,
+        'useCurve' => false,
         // 验证码图片高度
         'imageH'   => 40,
         // 验证码图片宽度
@@ -260,7 +260,9 @@ return [
         // 验证码位数
         'length'   => 4,
         // 验证成功后是否重置        
-        'reset'    => true
+        'reset'    => true,
+        // 添加杂点
+        'useNoise' => false,
     ],
 
 ];
