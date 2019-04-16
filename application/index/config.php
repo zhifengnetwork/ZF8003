9d4ab85c58c4925fc5249ba7d1fbb8ed3d4e53c7
zxz <1026853966@qq.com>
2019-04-16 18:03:48 +0800

    pc端套页面

---
+++ application/index/config.php
@@ -0,0 +1,47 @@
+<?php
+//配置文件
+return [
+    'view_replace_str'  =>  [
+        '__PUBLIC__'=> '/public/static',
+        '__STATIC__' => '/public/static/index',
+        '__ROOT__'=>'',
+        '__GOODSIMG__' => '/public/images/goods/',
+        '__IMAGES__' => '/public/images/',
+    ],
+
+    // +----------------------------------------------------------------------
+    // | 会话设置
+    // +----------------------------------------------------------------------
+
+    'session'                => [
+        'id'             => '',
+        // SESSION_ID的提交变量,解决flash上传跨域
+        'var_session_id' => '',
+        // SESSION 前缀
+        'prefix'         => 'index',
+        // 驱动方式 支持redis memcache memcached
+        'type'           => '',
+        // 是否自动开启 SESSION
+        'auto_start'     => true,
+    ],
+
+    // +----------------------------------------------------------------------
+    // | Cookie设置
+    // +----------------------------------------------------------------------
+    'cookie'                 => [
+        // cookie 名称前缀
+        'prefix'    => '',
+        // cookie 保存时间
+        'expire'    => 0,
+        // cookie 保存路径
+        'path'      => '/',
+        // cookie 有效域名
+        'domain'    => '',
+        //  cookie 启用安全传输
+        'secure'    => false,
+        // httponly设置
+        'httponly'  => '',
+        // 是否使用 setcookie
+        'setcookie' => true,
+    ],
+];
