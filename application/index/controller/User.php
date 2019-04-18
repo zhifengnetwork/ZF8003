94e379e5a889b5d8e87388d4d17c534e2967571e
670600971 <670600971@qq.com>
2019-04-18 21:22:07 +0800

    修改密码

--- application/index/controller/User.php
+++ application/index/controller/User.php
@@ -14,13 +14,31 @@ class User extends Base{
 
     public function index(){
 
+        // dump($this->user);
+        
 
 
+        $coupon = Db::name('user_coupon')->where('user_id',$this->user_id)->count(); 
+        $this->assign('coupon',$coupon);          
         $this->assign('info', $this->user);
         return $this->fetch();
     }
 
+    public function repwd(){
+       
+        
+        if($_POST){
+            $password = pwd_encryption(input('post.password'));
+            $user_id = $this->user_id;
+            $res = Db::name('users')->where('id',$user_id)->update(['password' => $password]);
+            if ($res) { 
+                return json(['status'=>1,'msg'=>'修改成功']);
+            }else{
+                return json(['status'=>0,'msg'=>'修改失败']);
+            }
+        }
 
+    }
 
 
 }
