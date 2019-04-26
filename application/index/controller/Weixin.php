bdad4d52190d148963dd12fde0817cb8a1d00c48
sonelyx <snowxtlyx@163.com>
2019-04-26 17:24:29 +0800

    微信服务器交互文件修改

--- application/index/controller/Weixin.php
+++ application/index/controller/Weixin.php
@@ -1,17 +1,45 @@
 <?php
 namespace app\index\controller;
+
 use think\Db;
+use think\Session;
+use think\Request;
 
 class Weixin{
 
+    public $weixin_config;
 
     public function native_notify(){
         
         $data = file_get_contents("php://input");
+
     	
 
     }
  
+
+    # 微信配置
+    public function wx_config(){
+        $config = Session::has('wx_config') ? Session::get('wx_config') : '';
+        if(!$config){
+            $config = Db::name('config')->where('type','weixin_config')->select();
+            foreach($config as $v){
+                $conf[$v['name']] = $v['value'];
+            }
+            Session::set('wx_config',$conf);
+            $config = $conf;
+        }
+        
+        if(!$config){
+
+            return layer_error('管理员未配置微信登录相关信息，功能未启用！');
+        }
+        
+
+        $this->weixin_config = $config;
+        return $config;
+    }
+
     public function xmlToArray($xml)
     {
     	$obj = simplexml_load_string($xml, 'SimpleXMLElement', LIBXML_NOCDATA);
