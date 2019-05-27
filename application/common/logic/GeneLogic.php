3eb5ab28821e4b69e258564f26f502b6a04c51cd
Rock-dav <1576648548@163.com>
2019-05-27 10:57:58 +0800

    更换pc端banner

---
+++ application/common/logic/GeneLogic.php
@@ -0,0 +1,29 @@
+<?php
+namespace app\common\logic;
+use think\Db;
+
+class GeneLogic
+{
+
+    public function __construct()
+    {
+        ini_set('memory_limit','2048M');
+        set_time_limit(0);
+    }
+
+
+    public function analysis()
+    {
+
+
+
+
+
+    }
+
+
+
+
+
+
+}
