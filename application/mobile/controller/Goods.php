373c87ad5c5b652b486a99be12cf2e6c8808cc4f
xiaozhi9752 <975247129@qq.com>
2019-07-10 12:17:45 +0800

    1

--- application/mobile/controller/Goods.php
+++ application/mobile/controller/Goods.php
@@ -36,6 +36,7 @@ class Goods extends Base
             'status' => 1,
         ];
         $where['type'] = ['neq',6];
+        $where['is_del'] = ['eq',0];
         $list = Db::name('goods')->where($where)->select();
         $this->assign('list', $list);
         return $this->fetch();
